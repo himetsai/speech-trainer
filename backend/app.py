94308d7ce4751df364fae934de8e7f337aa415b8
Pranay Jha <pranayjha@Pranays-MacBook-Air.local>
2023-10-28 13:42:17 -0700

    Whisper speech to text

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,18 @@
-from flask import Flask
-import together
 import os
-from flask import request
+
+import openai
+import together
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
 
 load_dotenv()
-
-together.api_key = os.environ.get("TOGETHER_API_KEY")
 
 app = Flask(__name__)
 
 @app.route('/api/feedback', methods=['POST'])
 def feedback():
+    together.api_key = os.environ.get("TOGETHER_API_KEY")
+
     data = request.json
 
     if not data:
@@ -34,5 +35,35 @@ def feedback():
     )
     return output['output']['choices'][0]['text']
 
+@app.route('/api/speech-to-text', methods=['POST'])
+def speech_to_text():
+    """
+    Request format: {"filepath": "/path/to/audio/file.mp3}
+    """
+    try:
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+        # Get the file path from the request
+        audio_filepath = request.json["filepath"]
+
+        # Check if the file exists
+        if not os.path.exists(audio_filepath):
+            return jsonify({'Error': 'File not found'}), 404
+        
+        file_size = os.path.getsize(audio_filepath)
+        
+        # Check if the file size is greater than 25 MB
+        if file_size > 25 * 1024 * 1024:
+            return jsonify({'error': 'File size exceeds 25 MB'}), 400
+        
+        audio_file = open(audio_filepath, "rb")
+        try:
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            return transcript["text"]
+        except Exception as e:
+            return jsonify({'Error': str(e)}), 404
+
+    except Exception as e:
+        return jsonify({'Error': str(e)}), 500
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
