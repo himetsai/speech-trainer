60e5d93a548fa272269955e2e7da177f88b3f452
Pranay Jha <pranayjha@Pranays-MacBook-Air.local>
2023-10-28 15:12:04 -0700

    XIlabs TTS

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import openai
 import together
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
+from elevenlabs import generate, save
 
 load_dotenv()
 
@@ -42,10 +43,9 @@ def speech_to_text():
     """
     try:
         openai.api_key = os.environ.get("OPENAI_API_KEY")
-        # Get the file path from the request
+
         audio_filepath = request.json["filepath"]
 
-        # Check if the file exists
         if not os.path.exists(audio_filepath):
             return jsonify({'Error': 'File not found'}), 404
         
@@ -62,6 +62,19 @@ def speech_to_text():
         except Exception as e:
             return jsonify({'Error': str(e)}), 404
 
+    except Exception as e:
+        return jsonify({'Error': str(e)}), 500
+
+@app.route('/api/text-to-speech', methods=['POST'])
+def text_to_speech():
+    """
+    Request format: {"text": "Hello World!", "voice": "Grace"}
+    """
+    try:
+        text, voice = request.json["text"], request.json["voice"]
+        audio_bytes = generate(text=text, voice=voice)
+        save(audio_bytes, "feedback.mp3")
+        return jsonify({})
     except Exception as e:
         return jsonify({'Error': str(e)}), 500
 
